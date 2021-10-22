0cc3eaa21eb67b13e93fe126c725dd4c66532beb
Dragibus <jeremiecool76700@hotmail.fr>
2021-10-22 19:47:49 +0200

    Build product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,57 @@
-// Check if the id parameter exist
+// Check Id parameter in URL
 function idVerification() {
-	let url = new URL(window.location.href);
-	let searchParams = new URLSearchParams(url.search);
-
-	if (searchParams.has("id")) {
-		let id = searchParams.get("id");
-		return id;
-	} else {
-		console.log("Erreur, il n'y a aucune correspondance.");
-	}
+  let url = new URL(window.location.href);
+  let searchParams = new URLSearchParams(url.search);
+  if (searchParams.has("id")) {
+    let id = searchParams.get("id");
+    return id;
+  } else {
+    console.log("Error, no Id found in the URL");
+  }
 }
+
+// Get informations for the specified product
+async function getInfoById() {
+  let id = idVerification();
+  try {
+    let response = await fetch(`http://localhost:3000/api/products/${id}`);
+    return await response.json();
+  } catch (error) {
+    console.log("Error : " + error);
+  }
+}
+
+// Render on Html
+(async function renderItem() {
+  let item = await getInfoById();
+  document.querySelector(
+    ".item__img"
+  ).innerHTML += `<img src="${item.imageUrl}" alt="${item.altTxt}">`;
+  document.getElementById("title").innerHTML += item.name;
+  document.getElementById("price").innerHTML += item.price;
+  document.getElementById("description").innerHTML += item.description;
+  // Choice of item colors
+  item.colors.forEach((color) => {
+    let htmlContent = `<option value="${color}">${color}</option>`;
+    document.getElementById("colors").innerHTML += htmlContent;
+  });
+})();
+
+// Add LocalStorage to card
+const addToCartBtn = document.getElementById("addToCart");
+addToCartBtn.addEventListener("click", () => {
+  const itemId = idVerification();
+  const itemColor = document.getElementById("colors").value;
+  const itemQuantity = document.getElementById("quantity").value;
+  // Confirm color and quantity != 0
+  if (itemColor === "") {
+    alert("Il est nécessaire de choisir une couleur");
+  } else if (itemQuantity == 0) {
+    alert("Il faut au moins ajouter un Kanap");
+  } else {
+    // Push in the localStorage
+    const itemInCart = [itemId, itemColor];
+    localStorage.setItem(itemInCart, itemQuantity);
+    window.location.href = "./cart.html";
+  }
+});
