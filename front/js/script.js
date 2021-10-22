f2d78eb110d19d8efd10253f3d55cfc1806bac41
Dragibus <jeremiecool76700@hotmail.fr>
2021-10-22 17:16:15 +0200

    Function of the render on Html + Calling the function

--- front/js/script.js
+++ front/js/script.js
@@ -7,3 +7,26 @@ async function getItems() {
 		console.log("Error : " + error);
 	}
 }
+
+// Function of the render on Html
+async function renderItems() {
+	let items = await getItems();
+	let htmlRender = "";
+	items.forEach((item) => {
+		let htmlContent = `
+		<a href="./product.html?id=${item._id}">
+			<article>
+				<img src="${item.imageUrl}" alt="${item.altTxt}">
+				<h3 class="productName">${item.name}</h3>
+				<p class="productDescription">${item.description}</p>
+			</article>
+		</a>
+		`;
+		htmlRender += htmlContent;
+	});
+	let itemContainer = document.getElementById("items");
+	itemContainer.innerHTML += htmlRender;
+}
+
+// Calling the function
+renderItems();
